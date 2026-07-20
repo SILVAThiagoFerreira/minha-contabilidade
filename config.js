98bfc29021554d2e436411cf7df16aa49f8549fa
SILVAThiagoFerreira <13452222+SILVAThiagoFerreira@users.noreply.github.com>
2026-07-20 10:29:19 +0100

    connect Pages to Apps Script spreadsheet backend

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // URL do Web App do Apps Script que grava os dados na planilha e na pasta do Drive.
 // O login do aplicativo continua sendo feito somente com usuário e senha locais.
 window.FINANCE_CONFIG = Object.freeze({
-  apiUrl: ""
+  apiUrl: "https://script.google.com/macros/s/AKfycbzQ8e8NP70tckxMN0qUI_9zg-ABo82g3dc7Wz4TY7XtTDJJGn8BQK_7jBFS3lwbVyJ3/exec"
 });
