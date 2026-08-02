a7855926d106867d22e873850b7464fc104115f9
SILVAThiagoFerreira <13452222+SILVAThiagoFerreira@users.noreply.github.com>
2026-08-02 15:06:25 +0100

    Aprimora perfil e identidade visual

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-// URL do Web App do Apps Script que grava os dados na planilha online.
+// URL pública do serviço de sincronização da aplicação.
 // O login e todos os dados dependem do backend online.
 window.FINANCE_CONFIG = Object.freeze({
     apiUrl: "https://script.google.com/macros/s/AKfycbzhH1juMTYr0sppMsMpHvFe1GXlGpTrq9hlQ6wYEREhM7iDkdd62Y0eaA5VvD4wfhAn/exec"
